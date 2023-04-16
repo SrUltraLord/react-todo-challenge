23fb8f20c82098fbf10bf6673e065e936df84119
SrUltraLord <davidrey8822@gmail.com>
2023-04-16 17:52:46 -0500

    fix: throw exception when not found in edit and delete.

--- server/src/services/todo.service.ts
+++ server/src/services/todo.service.ts
@@ -1,4 +1,5 @@
 import { PrismaClient, Prisma, Todo } from "@prisma/client";
+import { GraphQLError } from "graphql";
 import { v4 as uuidv4 } from "uuid";
 
 const prisma = new PrismaClient();
@@ -11,6 +12,21 @@ export const getAllTodos = async (
       complete: complete ?? undefined,
     },
   });
+};
+
+export const getTodoById = async (todoId: string): Promise<Todo | null> => {
+  const todo = await prisma.todo.findUnique({ where: { id: todoId } });
+
+  if (todo === null) {
+    throw new GraphQLError(`No se ha encontrado un todo con el id ${todoId}`, {
+      extensions: {
+        code: "NOT_FOUND",
+        status: 404,
+      },
+    });
+  }
+
+  return todo;
 };
 
 export const createTodo = async (
@@ -28,6 +44,8 @@ export const updateTodo = async (
   todoId: string,
   todo: Prisma.TodoUpdateInput
 ): Promise<string> => {
+  await getTodoById(todoId);
+
   const { id } = await prisma.todo.update({
     where: { id: todoId },
     data: todo,
@@ -37,6 +55,8 @@ export const updateTodo = async (
 };
 
 export const deleteTodo = async (todoId: string): Promise<boolean> => {
+  await getTodoById(todoId);
+
   const deletedTodo = await prisma.todo.delete({ where: { id: todoId } });
   return deletedTodo !== null;
 };
