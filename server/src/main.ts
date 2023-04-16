e107fc663e3dbba69694dafe32450e1a875df2f2
SrUltraLord <davidrey8822@gmail.com>
2023-04-16 15:10:19 -0500

    feat: added mutations to apollo server.

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,13 +1,13 @@
 import { ApolloServer } from "apollo-server";
 
 import { todoTypes } from "./schemas/todo.schema";
-import { Query, TodoMutationResolver } from "./resolvers/todo.resolvers";
+import { Query, Mutation } from "./resolvers/todo.resolvers";
 
 const server = new ApolloServer({
   typeDefs: todoTypes,
-  // schema: {},
   resolvers: {
     Query,
+    Mutation,
   },
 });
 
